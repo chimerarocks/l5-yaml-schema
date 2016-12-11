16ce441c7f7830d84980893d6b62b77899a6bf66
João Pedro <joaopedrodslv@gmail.com>
2016-12-11 03:52:20 -0200

    migration generator hotfix

--- src/ChimeraRocks/YamlSchema/Generators/MigrationGenerator.php
+++ src/ChimeraRocks/YamlSchema/Generators/MigrationGenerator.php
@@ -2,6 +2,7 @@
 
 namespace ChimeraRocks\YamlSchema\Generators;
 
+use Prettus\Repository\Generators\Generator;
 use Prettus\Repository\Generators\Migrations\NameParser;
 use ChimeraRocks\YamlSchema\Generators\SchemaParser;
 use Symfony\Component\HttpFoundation\File\Exception\FileNotFoundException;
